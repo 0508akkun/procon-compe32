ed65678b00550f6b7ecc55a2b2323808efb48f88
0508akkun <E17-ffkg@kure.kosen-ac.jp>
2021-09-21 14:58:49 +0900

    search fix image piece index

--- procon-compe32-gui/procon-32-gui/procon-32-gui/Solver.cpp
+++ procon-compe32-gui/procon-32-gui/procon-32-gui/Solver.cpp
@@ -40,6 +40,9 @@ Solver::Solver(Image image, int32 WH, int32 hdn, int32 vdn)
         }
     }
     graph = Array<Array<int32>>(board.size());  //エッジ分の領域を取る
+    graphMemo = Array<int32>(board.size(), -1);
+    prevPath = Array<int32>(Min(vdn, hdn), -1);
+    resultArray = Array<Array<int32>>(verDivNum, Array<int32>(horDivNum, -1));
 }
 
 Array<std::pair<int32, PieceInfo>> Solver::getBaseDiffList()
@@ -96,6 +99,7 @@ std::tuple<int32, PieceInfo, PieceInfo> Solver::searchConnectPiece(Array<std::pa
 void Solver::connectPiece(Array<std::tuple<int32, PieceInfo, PieceInfo>> cl)
 {
     int32 cornerIndex = -1; //隅に存在するピースのインデックスを記録する
+    int32 secondCornerIndex = -1;
     int32 subCornerIndex = -1;  //隅に存在するピースが見つからない時は1つのピースが連結している時に限り隅のインデックスに認定
     for (int32 i = 0; i < cl.size(); i++) {
         graph[std::get<1>(cl[i]).pieceId] << std::get<2>(cl[i]).pieceId;
@@ -109,27 +113,83 @@ void Solver::connectPiece(Array<std::tuple<int32, PieceInfo, PieceInfo>> cl)
             subCornerIndex = i;
         }
     }
+    if (cornerIndex != -1) {
+        secondCornerIndex = cornerDfs(cornerIndex, 0, graphMemo, prevPath);
+    }
+    else if(subCornerIndex != -1) {
+        secondCornerIndex = cornerDfs(subCornerIndex, 0, graphMemo, prevPath);
+    }
+    else {
+        secondCornerIndex = -1;
+        Print << U"正常にコーナーが検出できていません";
+    }
+    graphMemo = Array<int32>(board.size(), -1);
+    for (int32 i = 0; i < prevPath.size(); i++) {
+        graphMemo[prevPath[i]] = 1;
+    }
+    if (horDivNum <= verDivNum) {
+        for (int32 i = 1; i < horDivNum; i++) { //起点となる辺のインデックスを確定させる
+            resultArray[0][i] = prevPath[i];
+        }
+        for (int32 i = 0; i < verDivNum; i++) {
+            Array<int32> searchArray(horDivNum, -1);
+            for (int32 j = 1; j < horDivNum; j++) {
+                searchArray = indexDfs(resultArray[i][j], 0, graphMemo, prevPath, searchArray);
+                resultArray[i][j] = searchArray[j];
+            }
+        }
+    }
+    else {
+        for (int32 i = 0; i < verDivNum; i++) {
+            resultArray[i][0] = prevPath[i];
+        }
+        for (int32 i = 0; i < horDivNum; i++) {
+            Array<int32> searchArray(verDivNum, -1);
+            for (int32 j = 1; j < verDivNum; j++) {
+                searchArray = indexDfs(resultArray[j][i], 0, graphMemo, prevPath, searchArray);
+                resultArray[j][i] = searchArray[j];
+            }
+        }
+    }
 }
 
-int32 Solver::dfs(int32 index, int32 depth)
+int32 Solver::cornerDfs(int32 index, int32 depth, Array<int32> &gm, Array<int32> &pp)   //画像全体の短い辺を構成するピースを探索する
 {
     int32 cornerIndex = -1;
     int32 returnValue = -1;
+    if (gm[index] != -1)return;  //検索済みならば無視
+    if (depth == Min(horDivNum, verDivNum)) {   //最短のピースの分割数より深さが深くなった後にもう一つの隅は見つかっても隣り合う隅では無いのでその前に探索を終了
+        return -1;
+    }
+    gm[index] = 1;   //探索済みにする
+    pp[depth] = index;    //どのような経路を辿ったか保存
     if (graph[index].size() < 2 && depth == Min(horDivNum, verDivNum) - 1) {  //隅を見つけた場合、インデックスを戻り値に
         return index;
     }
-    if (depth == Min(horDivNum, verDivNum)) {   //最短のピースの数より深さが深くなってもう一つの隅は見つかっても隣り合う隅では無いのでその前に探索を終了
-        return -1;
-    }
-    depth++;
     for (int32 i = 0; i < graph[index].size(); i++) {
-        returnValue = dfs(graph[index][i], depth);
-        if (returnValue > -1) {
-            cornerIndex = returnValue;
-            return cornerIndex;
+        returnValue = cornerDfs(graph[index][i], depth++, gm, pp);  //隅のインデックスを見つける
+        if (returnValue > -1) { //見つけられたら
+            cornerIndex = returnValue;  //その隅を2つ目の隅と認定
+            return cornerIndex; //その値を返却する
         }
     }
-    return cornerIndex;
+    return cornerIndex; //答えを返却する
+}
+
+Array<int32> Solver::indexDfs(int32 index, int32 depth, Array<int32>& gm, Array<int32>& pp, Array<int32>& sa)    //インデックスがどこにあるのか判定する
+{
+    Array<int32> result;
+    if (gm[index] != -1)return sa;
+    if (depth == Min(horDivNum, verDivNum) + 1)return sa;
+    gm[index] = 1;  //探索済みにする
+    for (int32 i = 0; i < graph[index].size(); i++) {
+        if (pp[depth - 1] == graph[index][i]) {
+            sa[depth - 1] = index;  // resultArrayに結果を記録する
+            result = indexDfs(graph[index][i], depth++, gm, pp, sa);
+            return result;
+        }
+    }
+    return result;
 }
 
 PieceInfo::PieceInfo(int32 pi, int32 ei)
--- procon-compe32-gui/procon-32-gui/procon-32-gui/Solver.h
+++ procon-compe32-gui/procon-32-gui/procon-32-gui/Solver.h
@@ -9,12 +9,16 @@ class Solver
 	int32 horDivNum;
 	int32 verDivNum;
 	Array<Array<int32>> graph;
+	Array<int32> graphMemo;	//ˆê“x’Ê‚Á‚½Ž–‚ª‚ ‚é‚©”»’è
+	Array<int32> prevPath;	//‰ß‹Ž‚ÌŒo˜H‚ð•Û‘¶
+	Array<Array<int32>> resultArray;
 	Array<std::pair<int32, PieceInfo>> getBaseDiffList();
 	Array<std::pair<int32, int32>> classifyPiece();
 	Array<Array<Color>> edgePixelData;
 	std::tuple<int32, PieceInfo, PieceInfo> searchConnectPiece(Array<std::pair<int32, PieceInfo>>, PieceInfo bl);
 	void connectPiece(Array<std::tuple<int32, PieceInfo, PieceInfo>> cl);
-	int32 dfs(int32 index, int32 depth);
+	int32 cornerDfs(int32 index, int32 depth, Array<int32>& gm, Array<int32>& pp);
+	Array<int32> indexDfs(int32 index, int32 depth, Array<int32>& gm, Array<int32>& pp, Array<int32>& sa);
 public:
 	Solver(Image image, int32 WH, int32 hdn, int32 vdn);
 };
