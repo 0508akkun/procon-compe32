fba9b7a256a963b9c09673ad8b2eb761733fc9dc
0508akkun <E17-ffkg@kure.kosen-ac.jp>
2021-09-30 13:21:26 +0900

    fix solver bug

---
+++ procon32_sample/client.py
@@ -0,0 +1,13 @@
+import sys
+import urllib.request
+
+args = sys.argv
+
+add_str = args[1]   #通信する相手側のipアドレスを指定する
+
+# URL,保存するファイルのパスを指定
+url = "http://" + add_str + ":8080/result.txt" # URLを指定
+save_name = "result-gui.txt" #保存したい相対パスを指定
+
+# ダウンロードを実行
+urllib.request.urlretrieve(url, save_name)
